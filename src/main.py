87acdcbf313bb029b9b5f2374eb1eb3efb6da0f6
Samuel Knobloch <samuelknobloch@gmail.com>
2018-09-17 13:21:31 +0200

    RoboLab Autumn Course
    * Template commit (autumn 2017)

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import ev3dev.ev3 as ev3
+from hamming_code import HammingCode
+from stack_machine import StackMachine
+
+
+def run():
+    # the execution of all code shall be started from within this function
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    run()
